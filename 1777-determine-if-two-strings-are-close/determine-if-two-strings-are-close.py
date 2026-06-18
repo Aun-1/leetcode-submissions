06cdee831c3c4167a192eb232e81404896491fbf
Aun-1 <aun.creator.ai@gmail.com>
2026-06-18 17:53:06 +0500

    Time: 78 ms (84.67%) | Memory: 20.5 MB (32.73%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,28 +1,3 @@
-# class Solution:
-#     def closeStrings(self, word1: str, word2: str) -> bool:
-
-#         freq1 = [0] * 26
-#         freq2 = [0] * 26
-
-#         for ch in word1:
-#             freq1[ord(ch) - ord('a')] += 1
-
-#         for ch in word2:
-#             freq2[ord(ch) - ord('a')] += 1
-
-#         for i in range(26):
-#             if (freq1[i] == 0 and freq2[i] != 0) or (freq1[i] != 0 and freq2[i] == 0):
-#                 return False
-
-#         freq1.sort()
-#         freq2.sort()
-
-#         for i in range(26):
-#             if freq1[i] != freq2[i]:
-#                 return False
-
-#         return True
-
 from collections import Counter
 
 class Solution:
@@ -30,14 +5,15 @@ class Solution:
         if len(word1) != len(word2):
             return False
 
-        # count1 = Counter(word1) #O(n)
-        # count2 = Counter(word2) #O(m)
-        count1={}
-        count2={}
-        for c in word1:
-            count1[c]=count1.get(c,0)+1
-        for c in word2:
-            count2[c]=count2.get(c,0)+1
+        count1 = Counter(word1) #O(n)
+        count2 = Counter(word2) #O(m)
+        #same output as doing:
+        # count1={}
+        # count2={}
+        # for c in word1:
+        #     count1[c]=count1.get(c,0)+1
+        # for c in word2:
+        #     count2[c]=count2.get(c,0)+1
 
         #Same set of distinct characters used
         if set(count1.keys()) != set(count2.keys()):
