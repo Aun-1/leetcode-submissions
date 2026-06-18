2644325dd5517dc22035507f006b9544cbe819ce
Aun-1 <aun.creator.ai@gmail.com>
2026-06-18 21:44:20 +0500

    Time: 9 ms (59.63%) | Memory: 20.2 MB (43.97%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,24 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        # largest=0
+        # freq=[0]*58
+        # for c in s:
+        #     freq[ord(c)-ord('A')]+=1
+        #     if freq[ord(c)-ord('A')]+=1>largest:
+        #         largest=freq[ord(c)-ord('A')]
+            
+        freq={}
+        for c in s:
+            freq[c]=freq.get(c,0)+1
+
+        pairs=[]
+        for c in freq:
+            pairs.append((freq[c],c))
+
+        pairs.sort(reverse=True)
+
+        result=[]
+        for count,c in pairs:
+            result.append(c*count)
+
+        return ''.join(result)
