f772c5e68a48231dc7b414131857bc71a6e03bce
Aun-1 <aun.creator.ai@gmail.com>
2026-06-18 23:40:52 +0500

    Time: 4 ms (14.08%) | Memory: 19.5 MB (5.81%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        #check if even if even then sum of even +1
+        freq=[0]*58
+        for c in s:
+            freq[ord(c)-ord('A')]+=1
+        total=0
+        has_odd = False
+        for count in freq:
+            if count % 2 == 0:
+                total += count
+            if (count - 1) % 2 == 0:
+                total += count - 1
+                has_odd = True
+
+        return total + 1 if has_odd else total
