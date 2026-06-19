a463f941b09cc06e58be248e2ce1a90040d1f9f9
Aun-1 <aun.creator.ai@gmail.com>
2026-06-20 00:53:14 +0500

    Time: 3 ms (88.14%) | Memory: 19.4 MB (25.83%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
@@ -7,7 +7,7 @@ class Solution:
             diff2=i-k
             if diff1 in seen:
                 result+=seen[diff1]
-            if diff2 in seen:
+            if diff2 in seen:#not elif -> so both cases can be consdiered nums = [1,5,3], k=2-->Pairs: (1,3) diff=2 ✓, (5,3) diff=2 ✓ → answer should be 2
                 result+=seen[diff2]
             seen[i]=seen.get(i,0)+1
         return result
