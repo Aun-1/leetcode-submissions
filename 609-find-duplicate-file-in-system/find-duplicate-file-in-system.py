a6d3a171ffbe2b2f5d0eb8648f655631bde9d7b2
Aun-1 <aun.creator.ai@gmail.com>
2026-06-21 20:24:49 +0500

    Time: 21 ms (39.84%) | Memory: 26.5 MB (44.26%) - LeetSync

--- 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
@@ -1,21 +1,40 @@
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        new_list = []
-        for i in paths:             
-            data = i.split()         
-            new_list.append(data)
+        # res = defaultdict(list)
+        # new_list = []
+        # for i in paths:             
+        #     data = i.split()         
+        #     new_list.append(data)
 
-        for i in range(len(new_list)):
-            root_info = new_list[i][0]
-            for j in range(1, len(new_list[i])):
-                name, content = new_list[i][j].split('(')   # O(k)
-                key = content[:-1]                            # O(k)
-                value = root_info + '/' + name                # O(k)
-                res[key].append(value)
+        # for i in range(len(new_list)):
+        #     root_info = new_list[i][0]
+        #     for j in range(1, len(new_list[i])):
+        #         name, content = new_list[i][j].split('(')   # O(k)
+        #         key = content[:-1]                            # O(k)
+        #         value = root_info + '/' + name                # O(k)
+        #         res[key].append(value)
 
-        sol = []
-        for i in res:
-            if len(res[i]) > 1:      
-                sol.append(res[i])   
-        return sol
+        # sol = []
+        # for i in res:
+        #     if len(res[i]) > 1:      
+        #         sol.append(res[i])   
+        # return sol
+
+        #a more readable version of code would be 
+
+        content_to_paths = defaultdict(list)
+
+        for path in paths:
+            parts = path.split()
+            root = parts[0]
+            for file_info in parts[1:]:
+                name, content = file_info.split('(')
+                content = content[:-1]  # drop trailing ')'
+                content_to_paths[content].append(root + '/' + name)
+
+        result = []
+        for group in content_to_paths.values():
+            if len(group) > 1:
+                result.append(group)
+
+        return result
