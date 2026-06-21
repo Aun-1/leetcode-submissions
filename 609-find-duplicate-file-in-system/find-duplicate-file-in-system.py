1a333ead681cd7c5f17cfb495b1e2bd8ea28d15f
Aun-1 <aun.creator.ai@gmail.com>
2026-06-21 20:23:42 +0500

    Time: 20 ms (44.52%) | Memory: 27.9 MB (11.57%) - LeetSync

--- 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
@@ -1,22 +1,9 @@
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
-        # res = defaultdict(list)
-        # new_list=[]
-        # for i in paths:
-        #     data=i.split()
-        #     new_list.append(data)
-
-        # value_dict=[]
-        # for i in range(len(new_list)):
-        #     root_info=new_list[i][0]
-        #     for j in range(1,len(new_list[i])):
-        #         key=new_list[i][j][6:-1]
-        #         value=root_info+'/'+new_list[i][j][:5]
-        #         res[key].append(value)
         res = defaultdict(list)
         new_list = []
-        for i in paths:              # O(P) iterations, P = number of paths
-            data = i.split()         # O(L_i) - L_i = length of string i
+        for i in paths:             
+            data = i.split()         
             new_list.append(data)
 
         for i in range(len(new_list)):
@@ -29,6 +16,6 @@ class Solution:
 
         sol = []
         for i in res:
-            if len(res[i]) > 1:      # O(1) per check
-                sol.append(res[i])   # O(1) amortized, but copies a list reference
+            if len(res[i]) > 1:      
+                sol.append(res[i])   
         return sol
