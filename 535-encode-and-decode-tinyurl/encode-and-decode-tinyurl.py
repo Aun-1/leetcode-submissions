ed3068eb0a70b1d12ba4789082323961e8ad3cdd
Aun-1 <aun.creator.ai@gmail.com>
2026-06-22 20:44:48 +0500

    Time: 55 ms (10.57%) | Memory: 19.3 MB (64.19%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -1,21 +1,18 @@
 class Codec:
     def __init__(self):
-        self.encodeMap= {}
-        self.decodeMap = {}
-        self.base="http://tinyurl.com/"
+        self.url_map = {}
+        self.counter = 0
+
     def encode(self, longUrl: str) -> str:
-        """Encodes a URL to a shortened URL.
-        """
-        if longUrl not in self.encodeMap:
-            shortUrl=self.base + str(len(self.encodeMap)+1)
-            self.encodeMap[longUrl]=shortUrl
-            self.decodeMap[shortUrl]=longUrl
-        return self.encodeMap[longUrl]
+        """Encodes a URL to a shortened URL."""
+        self.counter += 1
+        shortUrl = "http://tinyurl.com/" + str(self.counter)
+        self.url_map[shortUrl] = longUrl
+        return shortUrl
 
     def decode(self, shortUrl: str) -> str:
-        """Decodes a shortened URL to its original URL.
-        """
-        return self.decodeMap[shortUrl]
+        """Decodes a shortened URL to its original URL."""
+        return self.url_map[shortUrl]
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
