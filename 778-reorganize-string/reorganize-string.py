c4de5f4739378812b4aa6193a1a434989e0cc8ba
Aun-1 <aun.creator.ai@gmail.com>
2026-06-27 12:51:53 +0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (51.50%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count={}
+        for c in s:
+            count[c]=count.get(c,0)+1
+        if max(count.values())>(len(s)+1)//2:
+            return ""
+        
+        result = [''] * len(s)
+        idx = 0
+        
+        for char, freq in sorted(count.items(), key=lambda x: -x[1]):
+            for _ in range(freq):
+                if idx >= len(s):
+                    idx = 1
+                result[idx] = char
+                idx += 2
+        
+        return "".join(result)
