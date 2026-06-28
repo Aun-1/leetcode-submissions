8a6b9b790c54b81200ae6049d2869c6532d3010b
Aun-1 <aun.creator.ai@gmail.com>
2026-06-28 17:07:55 +0500

    Time: 286 ms (99.82%) | Memory: 36.1 MB (85.95%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -1,21 +1,37 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        count = {}
-        for n in arr:
-            count[n] = count.get(n, 0) + 1
+        #works but below is alcearn approach 
+        # count = {}
+        # for n in arr:
+        #     count[n] = count.get(n, 0) + 1
         
-        sorted_items = sorted(count.items(), key=lambda x: x[1])
+        # sorted_items = sorted(count.items(), key=lambda x: x[1])
         
-        k_check = 0
-        for num, freq in sorted_items:
-            for _ in range(freq):
-                if k_check == k:  
-                    break
-                count[num] -= 1
-                k_check += 1
+        # k_check = 0
+        # for num, freq in sorted_items:
+        #     for _ in range(freq):
+        #         if k_check == k:  
+        #             break
+        #         count[num] -= 1
+        #         k_check += 1
 
-        result = 0
-        for c in count:
-            if count[c] > 0:
-                result += 1
-        return result
+        # result = 0
+        # for c in count:
+        #     if count[c] > 0:
+        #         result += 1
+        # return result
+
+        from collections import Counter
+        freq = Counter(arr)
+        counts = sorted(freq.values())  # ascending
+        
+        unique_remaining = len(counts)
+        
+        for c in counts:
+            if k >= c:
+                k -= c
+                unique_remaining -= 1
+            else:
+                break
+        
+        return unique_remaining
