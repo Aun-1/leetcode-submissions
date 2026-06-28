5e6639a7dff9e85e0dfd0d3ee27f746b4acc9a23
Aun-1 <aun.creator.ai@gmail.com>
2026-06-28 14:12:39 +0500

    Time: 263 ms (48.94%) | Memory: 30.9 MB (29.03%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -6,6 +6,12 @@ class Solution:
         best = 1
         for right in range(len(nums)):
             window_sum += nums[right]
+            '''
+If I want to make [1, 2, 4] all equal to 4...
+cost = (4-1) + (4-2) + (4-4)
+     = 4×3 - (1+2+4)
+     = target × window_size - window_sum
+            '''
             while nums[right] * (right - left + 1) - window_sum > k:
                 window_sum -= nums[left]
                 left += 1
