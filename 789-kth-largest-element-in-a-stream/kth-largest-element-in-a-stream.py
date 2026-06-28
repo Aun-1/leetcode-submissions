57666a510dc6b28cec527591626fd990fe517f86
Aun-1 <aun.creator.ai@gmail.com>
2026-06-28 23:28:42 +0500

    Time: 20 ms (42.31%) | Memory: 25.6 MB (16.16%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -16,7 +16,6 @@ class KthLargest:
         
         return self.heap[0]
 
-
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
