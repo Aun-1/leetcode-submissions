5063794644b00419168a8f24ffbcce376c3553bd
Aun-1 <aun.creator.ai@gmail.com>
2026-06-30 19:59:41 +0500

    Time: 35 ms (51.76%) | Memory: 21.6 MB (96.96%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,12 +1,10 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = {0: 1}  # prefix sum -> number of times seen
-        prefix_sum = 0
-        result = 0
-        for num in nums:
-            prefix_sum += num
-            # if (prefix_sum - k) has occurred before, that means
-            # there's a subarray ending here that sums to k
-            result += count.get(prefix_sum - k, 0)
-            count[prefix_sum] = count.get(prefix_sum, 0) + 1
-        return result
+        sum_record={0:1}
+        total=0
+        ans=0
+        for n in nums:
+            total+=n
+            ans+=sum_record.get(total-k,0)
+            sum_record[total]=sum_record.get(total,0)+1
+        return ans
