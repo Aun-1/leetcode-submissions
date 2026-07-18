9fcc515b3eab953d0465168504fe4d2b939467a6
Aun-1 <aun.creator.ai@gmail.com>
2026-07-18 16:51:21 +0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (38.13%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,22 +1,21 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        i = 0
-        while i < len(matrix):
-            if matrix[i][-1] < target:
-                i += 1
+        i=0
+        while i<len(matrix):
+            if matrix[i][-1]<target:
+                i+=1
             else:
                 break
         if i == len(matrix):
             return False   # target bigger than everything
-
-        lo = 0
-        hi = len(matrix[i]) - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if matrix[i][mid] > target:
-                hi = mid - 1
-            elif matrix[i][mid] < target:
-                lo = mid + 1
+        lo=0
+        hi=len(matrix[i])-1
+        while lo<=hi:
+            mid=(lo+hi)//2
+            if matrix[i][mid]>target:
+                hi=mid-1
+            elif matrix[i][mid]<target:
+                lo=mid+1
             else:
                 return True
         return False
