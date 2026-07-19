bb8760a7e37e4f75dfe5439b19a04d5fc2ec18ef
Aun-1 <aun.creator.ai@gmail.com>
2026-07-19 11:41:14 +0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (99.99%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -2,21 +2,27 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m, n = len(matrix), len(matrix[0])
 
-        lo, hi = 0, m - 1
+        lo, hi = 0, m
         while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if matrix[mid][-1] >= target:
-                hi = mid
+            mid = (hi + lo) // 2
+            if matrix[mid][-1] < target:
+                lo = mid + 1   # ruled out -> shrink left
             else:
-                lo = mid + 1
+                hi = mid         # candidate -> shrink right
         row = lo
 
-        lo, hi = 0, n - 1
+        if row == m:
+            return False
+
+        lo, hi = 0, n -1
         while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if matrix[row][mid] >= target:
-                hi = mid
+            mid = (hi + lo) // 2
+            if matrix[row][mid] < target:
+                lo = mid + 1   # ruled out -> shrink left
             else:
-                lo = mid + 1
+                hi = mid      # candidate -> shrink right
+
+        if lo == n:
+            return False
 
         return matrix[row][lo] == target
