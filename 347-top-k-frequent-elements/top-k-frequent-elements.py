1cd2b52ae4eac32d6c100c3583ca94fe72b6f987
Aun-1 <aun.creator.ai@gmail.com>
2026-09-12 23:10:40 -0400

    Time: 7 ms (52.30%) | Memory: 22.7 MB (94.20%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,29 @@
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # numCount = {}
+        # for n in nums:
+        #     if n not in numCount:
+        #         numCount[n]=1
+        #     else:
+        #         numCount[n]+=1
+        
+        # heap = []
+        # for num, count in numCount.items():
+        #     heapq.heappush(heap, (count, num))
+        #     if len(heap) > k:
+        #         heapq.heappop(heap)
+        
+        # result = []
+        # for pair in heap:
+        #     result.append(pair[1])
+        # return result
+
         numCount = {}
         for n in nums:
             if n not in numCount:
                 numCount[n]=1
             else:
                 numCount[n]+=1
-        
-        heap = []
-        for num, count in numCount.items():
-            heapq.heappush(heap, (count, num))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        
-        result = []
-        for pair in heap:
-            result.append(pair[1])
-        return result
+        ordered = sorted(numCount, key=numCount.get, reverse=True)
+        return ordered[:k]
