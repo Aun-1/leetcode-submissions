ec27fcbc2fa18fe450c43a5942513eaf6892e9a7
Aun-1 <aun.creator.ai@gmail.com>
2026-09-12 21:30:36 -0400

    Time: 11 ms (77.52%) | Memory: 19.4 MB (46.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        if len(s) != len(t):
+            return False
+        
+        arr=[0]*26
+
+        for i in range(len(s)):
+            arr[ord(s[i])-ord('a')]+=1
+            arr[ord(t[i])-ord('a')]-=1
+
+        for val in arr:
+            if val!=0:
+                return False
+        return True
